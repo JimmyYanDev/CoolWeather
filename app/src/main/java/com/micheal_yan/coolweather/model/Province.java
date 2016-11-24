6c028f04f3136808fcca03e782fcdfa5b3096633
Michael-Yan <956462326@qq.com>
2016-11-24 13:11:22 +0800

    数据库相关操作

---
+++ app/src/main/java/com/micheal_yan/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.micheal_yan.coolweather.model;
+
+/**
+ * Created by micheal-yan on 2016/11/24.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
